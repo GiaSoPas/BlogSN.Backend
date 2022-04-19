1afd04e1f29591f567302f5626a65af42905c9e9
CucerOriginal <87267507+CucerOriginal@users.noreply.github.com>
2022-04-20 00:50:00 +0300

    Больше данных

--- Identity/DbContext/ApplicationDbContext.cs
+++ Identity/DbContext/ApplicationDbContext.cs
@@ -28,7 +28,8 @@ namespace Identity.DbContext
             builder.Entity<Post>().HasData(
                 new Post { Id = 1, Title = "Спорт", Content = "Про спорт и все такое", CategoryId = 1 , ApplicationUserId = "88aec81d-b5b0-45f3-8721-8d41560b02f7" },
                 new Post { Id = 2, Title = "Киберспорт", Content = "Про киберспорт и все такое", CategoryId = 2, ApplicationUserId = "88aec81d-b5b0-45f3-8721-8d41560b02f7" },
-                new Post { Id = 3, Title = "Cпортмашины", Content = "Про спортмашины и все такое", CategoryId = 3, ApplicationUserId = "88aec81d-b5b0-45f3-8721-8d41560b02f7" });
+                new Post { Id = 3, Title = "Киберспорт", Content = "Про киберспорт и все такое", CategoryId = 2, ApplicationUserId = "88aec81d-b5b0-45f3-8721-8d41560b02f7" },
+                new Post { Id = 4, Title = "Cпортмашины", Content = "Про спортмашины и все такое", CategoryId = 3, ApplicationUserId = "88aec81d-b5b0-45f3-8721-8d41560b02f7" });
         }
         
     }
