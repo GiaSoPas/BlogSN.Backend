7050180ac01c318f6b3a47183c26009541c08991
CucerOriginal <87267507+CucerOriginal@users.noreply.github.com>
2022-05-03 14:42:46 +0300

    Добавил поиск пользователя по id

---
+++ BlogSN.Backend/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using BlogSN.Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Models.ModelsIdentity.IdentityAuth;
+
+namespace BlogSN.Backend.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUsersServive _service;
+
+        public UserController(IUsersServive service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ApplicationUser))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ApplicationUser>> GetPost(string id, CancellationToken cancellationToken)
+        {
+            var user = await _service.GetUserById(id, cancellationToken);
+
+            return user;
+        }
+    }
+}
---
+++ BlogSN.Backend/Services/IUsersServive.cs
@@ -0,0 +1,9 @@
+﻿using Models.ModelsIdentity.IdentityAuth;
+
+namespace BlogSN.Backend.Services
+{
+    public interface IUsersServive
+    {
+        public Task<ApplicationUser> GetUserById(string id, CancellationToken cancellationToken);
+    }
+}
