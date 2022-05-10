dcee5783aee09e6e372509ea9b90a414056aa602
CucerOriginal <87267507+CucerOriginal@users.noreply.github.com>
2022-05-10 14:52:20 +0300

    исправил счетчик комментов

--- BlogSN.Backend/Services/CommentService.cs
+++ BlogSN.Backend/Services/CommentService.cs
@@ -1,5 +1,6 @@
 ﻿using BlogSN.Backend.Data;
 using BlogSN.Backend.Exceptions;
+using BlogSN.Models;
 using Microsoft.EntityFrameworkCore;
 using Models.ModelsBlogSN;
 
@@ -17,11 +18,7 @@ namespace BlogSN.Backend.Services
         public async Task CreateComment(Comment comment, CancellationToken cancellationToken)
         {
             await _context.Comment.AddAsync(comment, cancellationToken);
-            var post = await _context.Post.FirstOrDefaultAsync(p => p.Id == comment.PostId);
-            if (post is null)
-            {
-                throw new NotFoundException($"No post with id");
-            }
+            var post = await GetPostById(comment.PostId, cancellationToken);
             post.CommentsCount++;
             await _context.SaveChangesAsync();
         }
@@ -38,15 +35,31 @@ namespace BlogSN.Backend.Services
 
         public async Task DeleteCommentById(int id, CancellationToken cancellationToken)
         {
-            var comment = await _context.Comment.FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+            var comment = await GetCommentById(id, cancellationToken);
+            var post = await GetPostById(comment.PostId, cancellationToken);
+            post.CommentsCount--;
+            _context.Comment.Remove(comment);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
 
+        public async Task<Comment> GetCommentById(int id, CancellationToken cancellationToken)
+        {
+            var comment = await _context.Comment.FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
             if (comment is null)
             {
                 throw new NotFoundException($"No comment with id = {id}");
             }
+            return comment;
+        }
 
-            _context.Comment.Remove(comment);
-            await _context.SaveChangesAsync(cancellationToken);
+        public async Task<Post> GetPostById(int id, CancellationToken cancellationToken)
+        {
+            var post = await _context.Post.FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+            if (post is null)
+            {
+                throw new NotFoundException($"No post with id = {id}");
+            }
+            return post;
         }
     }
 }
